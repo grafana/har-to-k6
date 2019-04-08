139a35e8bbb8aa2d7613b9e62b213f0b2b5d1c4b
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-08 11:32:25 -0400

    Validate header

---
+++ src/validate/header.js
@@ -0,0 +1,40 @@
+const { empty } = require('../aid')
+const { InvalidArchiveError } = require('../error')
+
+/*
+ * name: require string
+ * value: optional string
+ * comment: optional string
+ */
+function header (node, i, j, assay) {
+  validate(node, i, j)
+}
+
+function validate (node, i, j) {
+  if (empty(node.name)) {
+    throw new InvalidArchiveError(
+      { name: 'MissingHeaderName' },
+      `Missing header name (${i}:${j})`
+    )
+  }
+  if (typeof node.name !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidHeaderName' },
+      `Invalid header name (${i}:${j}): must be string`
+    )
+  }
+  if (node.value && typeof node.value !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidHeaderValue' },
+      `Invalid header value (${i}:${j}): must be string`
+    )
+  }
+  if (node.comment && typeof node.comment !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidComment' },
+      `Invalid header comment (${i}:${j}): must be string`
+    )
+  }
+}
+
+module.exports = header
---
+++ test/unit/validate/header.js
@@ -0,0 +1,49 @@
+import test from 'ava'
+import header from 'validate/header'
+import { makeAssay } from 'aid'
+
+test('missing name', t => {
+  t.throws(() => {
+    header({}, 0, 0, makeAssay())
+  }, { name: 'MissingHeaderName' })
+})
+
+test('invalid name', t => {
+  t.throws(() => {
+    header({ name: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidHeaderName' })
+})
+
+test('invalid value', t => {
+  t.throws(() => {
+    header({ name: 'Accept', value: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidHeaderValue' })
+})
+
+test('invalid comment', t => {
+  t.throws(() => {
+    header({ name: 'Accept', comment: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidComment' })
+})
+
+test('valid minimal', t => {
+  t.notThrows(() => {
+    header({ name: 'Accept' }, 0, 0, makeAssay())
+  })
+})
+
+test('valid empty value', t => {
+  t.notThrows(() => {
+    header({ name: 'Accept', value: '' }, 0, 0, makeAssay())
+  })
+})
+
+test('valid full', t => {
+  t.notThrows(() => {
+    header({
+      name: 'Accept',
+      value: '*/*',
+      comment: 'Accept everything'
+    }, 0, 0, makeAssay())
+  })
+})
