8054966439aa4b74c0c16e40e88c923ff811f188
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-08 11:21:34 -0400

    Validate queryItem

---
+++ test/unit/validate/queryItem.js
@@ -0,0 +1,49 @@
+import test from 'ava'
+import queryItem from 'validate/queryItem'
+import { makeAssay } from 'aid'
+
+test('missing name', t => {
+  t.throws(() => {
+    queryItem({}, 0, 0, makeAssay())
+  }, { name: 'MissingQueryItemName' })
+})
+
+test('invalid name', t => {
+  t.throws(() => {
+    queryItem({ name: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidQueryItemName' })
+})
+
+test('invalid value', t => {
+  t.throws(() => {
+    queryItem({ name: 'search', value: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidQueryItemValue' })
+})
+
+test('invalid comment', t => {
+  t.throws(() => {
+    queryItem({ name: 'search', comment: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidComment' })
+})
+
+test('valid minimal', t => {
+  t.notThrows(() => {
+    queryItem({ name: 'search' }, 0, 0, makeAssay())
+  })
+})
+
+test('valid empty value', t => {
+  t.notThrows(() => {
+    queryItem({ name: 'search', value: '' }, 0, 0, makeAssay())
+  })
+})
+
+test('valid full', t => {
+  t.notThrows(() => {
+    queryItem({
+      name: 'search',
+      value: 'kitten',
+      comment: 'Typical search'
+    }, 0, 0, makeAssay())
+  })
+})
