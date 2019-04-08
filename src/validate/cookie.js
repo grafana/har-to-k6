a9eb6845eefe4ae5ab1c17b79962905de36cd9f7
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-08 12:19:20 -0400

    Validate cookie

---
+++ src/validate/cookie.js
@@ -0,0 +1,85 @@
+const iso8601 = require('iso8601-validator')
+const { empty } = require('../aid')
+const { InvalidArchiveError } = require('../error')
+
+/*
+ * name: required string
+ * value: optional string
+ * path: optional string
+ * domain: optional string
+ * expires: optional string, ISO 8601 format
+ * httpOnly: optional boolean
+ * secure: optional boolean
+ * comment: optional string
+ */
+function cookie (node, i, j, assay) {
+  validate(node, i, j)
+}
+
+function validate (node, i, j) {
+  if (empty(node.name)) {
+    throw new InvalidArchiveError(
+      { name: 'MissingCookieName' },
+      `Missing cookie name (${i}:${j})`
+    )
+  }
+  if (typeof node.name !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidCookieName' },
+      `Invalid cookie name (${i}:${j}): must be string`
+    )
+  }
+  if (node.value && typeof node.value !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidCookieValue' },
+      `Invalid cookie value (${i}:${j}): must be string`
+    )
+  }
+  if (node.path && typeof node.path !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidCookiePath' },
+      `Invalid cookie path (${i}:${j}): must be string`
+    )
+  }
+  if (node.domain && typeof node.domain !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidCookieDomain' },
+      `Invalid cookie domain (${i}:${j}): must be string`
+    )
+  }
+  if (node.expires) {
+    if (typeof node.expires !== 'string') {
+      throw new InvalidArchiveError(
+        { name: 'InvalidCookieExpiration' },
+        `Invalid cookie expiration (${i}:${j}): must be string`
+      )
+    }
+
+    if (!iso8601.test(node.expires)) {
+      throw new InvalidArchiveError(
+        { name: 'InvalidCookieExpiration' },
+        `Invalid cookie expiration (${i}:${j}): must be ISO 8601 datetime`
+      )
+    }
+  }
+  if (!(empty(node.httpOnly) || typeof node.httpOnly === 'boolean')) {
+    throw new InvalidArchiveError(
+      { name: 'InvalidCookieHttpOnly' },
+      `Invalid cookie HTTP only flag (${i}:${j}): must be boolean`
+    )
+  }
+  if (!(empty(node.secure) || typeof node.secure === 'boolean')) {
+    throw new InvalidArchiveError(
+      { name: 'InvalidCookieSecure' },
+      `Invalid cookie secure flag (${i}:${j}): must be boolean`
+    )
+  }
+  if (node.comment && typeof node.comment !== 'string') {
+    throw new InvalidArchiveError(
+      { name: 'InvalidComment' },
+      `Invalid cookie comment (${i}:${j}): must be string`
+    )
+  }
+}
+
+module.exports = cookie
---
+++ test/unit/validate/cookie.js
@@ -0,0 +1,96 @@
+import test from 'ava'
+import cookie from 'validate/cookie'
+import { makeAssay } from 'aid'
+
+test('missing name', t => {
+  t.throws(() => {
+    cookie({}, 0, 0, makeAssay())
+  }, { name: 'MissingCookieName' })
+})
+
+test('invalid name', t => {
+  t.throws(() => {
+    cookie({ name: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidCookieName' })
+})
+
+test('invalid value', t => {
+  t.throws(() => {
+    cookie({ name: 'session', value: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidCookieValue' })
+})
+
+test('invalid path', t => {
+  t.throws(() => {
+    cookie({ name: 'session', path: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidCookiePath' })
+})
+
+test('invalid domain', t => {
+  t.throws(() => {
+    cookie({ name: 'session', domain: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidCookieDomain' })
+})
+
+test('invalid expires type', t => {
+  t.throws(() => {
+    cookie({ name: 'session', expires: 5 }, 0, 0, makeAssay())
+  }, {
+    name: 'InvalidCookieExpiration',
+    message: 'Invalid cookie expiration (0:0): must be string'
+  })
+})
+
+test('invalid expires format', t => {
+  t.throws(() => {
+    cookie({ name: 'session', expires: '5 Feb 2020' }, 0, 0, makeAssay())
+  }, {
+    name: 'InvalidCookieExpiration',
+    message: 'Invalid cookie expiration (0:0): must be ISO 8601 datetime'
+  })
+})
+
+test('invalid httpOnly', t => {
+  t.throws(() => {
+    cookie({ name: 'session', httpOnly: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidCookieHttpOnly' })
+})
+
+test('invalid secure', t => {
+  t.throws(() => {
+    cookie({ name: 'session', secure: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidCookieSecure' })
+})
+
+test('invalid comment', t => {
+  t.throws(() => {
+    cookie({ name: 'session', comment: 5 }, 0, 0, makeAssay())
+  }, { name: 'InvalidComment' })
+})
+
+test('valid minimal', t => {
+  t.notThrows(() => {
+    cookie({ name: 'session' }, 0, 0, makeAssay())
+  })
+})
+
+test('valid empty value', t => {
+  t.notThrows(() => {
+    cookie({ name: 'session', value: '' }, 0, 0, makeAssay())
+  })
+})
+
+test('valid full', t => {
+  t.notThrows(() => {
+    cookie({
+      name: 'session',
+      value: '123876',
+      path: '/',
+      domain: 'example.com',
+      expires: new Date().toISOString(),
+      httpOnly: true,
+      secure: true,
+      cookie: 'Perform operation under an active session'
+    }, 0, 0, makeAssay())
+  })
+})
