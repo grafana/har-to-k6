7452c4d65b14ac123cf2213aa0722645b2505ab9
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-11 10:57:02 -0400

    Pass result in root parse test

--- test/unit/parse/root.js
+++ test/unit/parse/root.js
@@ -1,8 +1,9 @@
 import test from 'ava'
 import isolate from 'helper/isolate'
+import { makeResult } from 'aid'
 const [ root, { log } ] = isolate(test, 'parse/root', { log: 'parse/log' })
 
 test.serial('success', t => {
-  root({ log: {} })
+  root({ log: {} }, makeResult())
   t.true(log.calledOnce)
 })
