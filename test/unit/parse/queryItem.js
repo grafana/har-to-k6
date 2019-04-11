eb6f6115afc593354f426b16ce9d14766448f948
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-11 14:03:32 -0400

    Parse queryItem

---
+++ test/unit/parse/queryItem.js
@@ -0,0 +1,36 @@
+import test from 'ava'
+import queryItem from 'parse/queryItem'
+
+function makeSpec () {
+  return new Map()
+}
+
+test('minimal', t => {
+  const spec = makeSpec()
+  queryItem({ name: 'search' }, spec)
+  t.deepEqual(
+    spec,
+    new Map()
+      .set('search', new Set([ {} ]))
+  )
+})
+
+test('value', t => {
+  const spec = makeSpec()
+  queryItem({ name: 'search', value: 'kittens' }, spec)
+  t.deepEqual(
+    spec,
+    new Map()
+      .set('search', new Set([ { value: 'kittens' } ]))
+  )
+})
+
+test('comment', t => {
+  const spec = makeSpec()
+  queryItem({ name: 'search', comment: 'Test a search' }, spec)
+  t.deepEqual(
+    spec,
+    new Map()
+      .set('search', new Set([ { comment: 'Test a search' } ]))
+  )
+})
