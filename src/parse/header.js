5586bbc53b26016c72f29f2860c6282f4f4bb0de
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-11 14:29:23 -0400

    Parse header

---
+++ src/parse/header.js
@@ -0,0 +1,15 @@
+function header (node, spec) {
+  const item = {}
+  if (node.value) {
+    item.value = node.value
+  }
+  if (node.comment) {
+    item.comment = node.comment
+  }
+  if (!spec.has(node.name)) {
+    spec.set(node.name, new Set())
+  }
+  spec.get(node.name).add(item)
+}
+
+module.exports = header
