0239371df7acfe1977749391e13fe232e1ea789d
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-11 10:54:50 -0400

    Test log.creator parsing

--- test/unit/parse/log.js
+++ test/unit/parse/log.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import isolate from 'helper/isolate'
 import { makeResult } from 'aid'
-const [ log, { entries, pages } ] =
+const [ log, { creator, entries, pages } ] =
   isolate(test, 'parse/log', {
     browser: 'parse/browser',
     creator: 'parse/creator',
@@ -25,6 +25,11 @@ test.serial('comment', t => {
   const result = makeResult()
   log({ comment: 'Sent from my iPad' }, result)
   t.is(result.comment[1], 'Sent from my iPad')
+})
+
+test.serial('creator', t => {
+  log({ creator: {} }, makeResult())
+  t.true(creator.calledOnce)
 })
 
 test.serial('pages', t => {
