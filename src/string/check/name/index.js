639b58302280d159a71d9962c4db6916cc2fff01
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-13 11:04:50 -0400

    Define check name

---
+++ src/string/check/name/index.js
@@ -0,0 +1,26 @@
+const JSONPath = require('./JSONPath')
+const JSONPathValue = require('./JSONPathValue')
+const Regex = require('./Regex')
+const Text = require('./Text')
+const { CheckType } = require('../../../enum')
+const { UnrecognizedError } = require('../../../error')
+
+function name (node) {
+  switch (node.type) {
+    case CheckType.JSONPath:
+      return JSONPath(node)
+    case CheckType.JSONPathValue:
+      return JSONPathValue(node)
+    case CheckType.Regex:
+      return Regex(node)
+    case CheckType.Text:
+      return Text(node)
+    default:
+      throw new UnrecognizedError(
+        { name: 'UnrecognizedCheckType' },
+        `Unrecognized check type: ${node.type}`
+      )
+  }
+}
+
+module.exports = name
---
+++ test/unit/string/check/name/index.js
@@ -0,0 +1,62 @@
+import test from 'ava'
+import isolate from 'helper/isolate'
+import { extrinsic } from 'aid'
+import { CheckCondition, CheckSubject, CheckType } from 'enum'
+const [ checkName, { JSONPath, JSONPathValue, Regex, Text } ] =
+  isolate(test, 'string/check/name', {
+    JSONPath: 'string/check/name/JSONPath',
+    JSONPathValue: 'string/check/name/JSONPathValue',
+    Regex: 'string/check/name/Regex',
+    Text: 'string/check/name/Text'
+  })
+
+test.serial('JSONPath', t => {
+  const result = Symbol('result')
+  JSONPath.returns(result)
+  const name = checkName({
+    type: CheckType.JSONPath,
+    expression: 'result.token'
+  })
+  t.is(name, result)
+})
+
+test.serial('JSONPathValue', t => {
+  const result = Symbol('result')
+  JSONPathValue.returns(result)
+  const name = checkName({
+    type: CheckType.JSONPathValue,
+    expression: 'user.id',
+    condition: CheckCondition.Equals,
+    value: '8734'
+  })
+  t.is(name, result)
+})
+
+test.serial('Regex', t => {
+  const result = Symbol('result')
+  Regex.returns(result)
+  const name = checkName({
+    type: CheckType.Regex,
+    subject: CheckSubject.HttpStatusCode,
+    expression: '2\\d\\d'
+  })
+  t.is(name, result)
+})
+
+test.serial('Text', t => {
+  const result = Symbol('result')
+  Text.returns(result)
+  const name = checkName({
+    type: CheckType.Text,
+    subject: CheckSubject.ResponseBody,
+    condition: CheckCondition.StartsWith,
+    value: 'Success'
+  })
+  t.is(name, result)
+})
+
+test.serial('invalid', t => {
+  t.throws(() => {
+    checkName({ type: extrinsic(CheckType) })
+  }, { name: 'UnrecognizedCheckType' })
+})
