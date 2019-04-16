80626ca15978598491dcd6243734773025a2ae78
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-16 17:27:14 -0400

    Order object items for rendering

---
+++ src/render/object/order.js
@@ -0,0 +1,41 @@
+const stringSpecies = require('../../species/string')
+const { StringSpecies } = require('../../enum')
+const { UnrecognizedError } = require('../../error')
+
+function order (items) {
+  items.sort(sort)
+}
+
+function sort (a, b) {
+  const aSpecies = stringSpecies(a.key)
+  const bSpecies = stringSpecies(b.key)
+  if (aSpecies === bSpecies) {
+    return a.key > b.key ? 1 : a.key < b.key ? -1 : 0
+  } else {
+    switch (aSpecies) {
+      case StringSpecies.Identifier:
+        return -1
+      case StringSpecies.String:
+        switch (bSpecies) {
+          case StringSpecies.Identifier:
+            return 1
+          case StringSpecies.Template:
+            return -1
+          default:
+            throw new UnrecognizedError(
+              { name: 'UnrecognizedStringSpecies' },
+              `Unrecognized string species: ${bSpecies}`
+            )
+        }
+      case StringSpecies.Template:
+        return 1
+      default:
+        throw new UnrecognizedError(
+          { name: 'UnrecognizedStringSpecies' },
+          `Unrecognized string species: ${aSpecies}`
+        )
+    }
+  }
+}
+
+module.exports = order
