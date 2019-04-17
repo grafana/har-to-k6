60da1d7f636716233c70591d773e4b798b154104
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-17 14:21:11 -0400

    Correct fixed address test input

--- test/unit/render/address/fixed.js
+++ test/unit/render/address/fixed.js
@@ -1,12 +1,17 @@
 import test from 'ava'
 import isolate from 'helper/isolate'
-import { requestFactor as makeRequestFactor } from 'make'
+import {
+  requestFactor as makeRequestFactor,
+  requestSpec as makeRequestSpec
+} from 'make'
 const [ fixed, { string } ] =
   isolate(test, 'render/address/fixed', { string: 'render/string' })
 
 test('basic', t => {
   string.returns('"http://example.com"')
   const factor = makeRequestFactor()
-  fixed('http://example.com', factor)
+  const spec = makeRequestSpec()
+  spec.address = 'http://example.com'
+  fixed(spec, factor)
   t.is(factor.address, '"http://example.com"')
 })
