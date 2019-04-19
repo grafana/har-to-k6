19498a6b88d86e6125790267883093fab4321b49
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-19 19:48:03 -0400

    Render cookies

---
+++ src/render/cookies.js
@@ -0,0 +1,13 @@
+const cookie = require('./cookie')
+const object = require('./object')
+
+function cookies (spec) {
+  if (spec.size) {
+    const entries = [ ...spec ].map(([ name, item ]) => cookie(name, item))
+    return object(entries)
+  } else {
+    return null
+  }
+}
+
+module.exports = cookies
