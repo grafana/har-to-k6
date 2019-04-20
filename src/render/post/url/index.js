316bff655b1d1ed51ccdea007a1e39c19d81b455
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-20 19:08:04 -0400

    Render URL encoded post

---
+++ src/render/post/url/index.js
@@ -0,0 +1,12 @@
+const plural = require('./plural')
+const singular = require('./singular')
+
+function url (spec) {
+  if (spec.state.params.plural) {
+    return plural(spec)
+  } else {
+    return singular(spec.post.params)
+  }
+}
+
+module.exports = url
