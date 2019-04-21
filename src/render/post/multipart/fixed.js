a3d0785fc84b06b1f95659f4bdd2c2e43f9a459f
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-04-21 13:59:23 -0400

    Render multipart fixed post comment

--- src/render/post/multipart/fixed.js
+++ src/render/post/multipart/fixed.js
@@ -1,8 +1,17 @@
+const comment = require('../../comment')
 const MimeBuilder = require('emailjs-mime-builder').default
+const note = require('../../note/map')
 const string = require('../../string')
 
 // Multipart encoded post data without variable
 function fixed (params) {
+  return [
+    description(params),
+    value(params)
+  ].filter(item => item).join(`\n`)
+}
+
+function value (params) {
   const message = new MimeBuilder('multipart/form-data')
   for (const [ name, items ] of params) {
     for (const item of items) {
@@ -13,6 +22,15 @@ function fixed (params) {
   }
   const encoded = message.build()
   return string(encoded)
+}
+
+function description (params) {
+  const content = note(params)
+  if (content) {
+    return comment(content)
+  } else {
+    return null
+  }
 }
 
 module.exports = fixed
--- test/int/render/post/multipart/fixed.js
+++ test/int/render/post/multipart/fixed.js
@@ -1,13 +1,16 @@
 import test from 'ava'
 import isolate from 'helper/isolate'
-const [ fixed, { string } ] =
-  isolate(test, 'render/post/multipart/fixed', { string: 'render/string' })
+const [ fixed, { comment, note, string } ] =
+  isolate(test, 'render/post/multipart/fixed', {
+    comment: 'render/comment',
+    note: 'render/note/map',
+    string: 'render/string'
+  })
 
 test.serial('result', t => {
-  const rendered = Symbol('rendered')
-  string.returns(rendered)
+  string.returns('rendered')
   const result = fixed(new Map())
-  t.is(result, rendered)
+  t.is(result, 'rendered')
 })
 
 test.serial('build', t => {
@@ -27,4 +30,18 @@ test.serial('field', t => {
   t.is(result[7], 'Content-Disposition: form-data; name=search')
   t.is(result[8], 'Content-Transfer-Encoding: base64')
   t.is(result[10], 'a2l0dGVu')
+})
+
+test.serial('comment', t => {
+  note.returns('-search- Find kittens')
+  comment.returns('// -search- Find kittens')
+  string.returns('"search=kitten"')
+  const params = new Map()
+    .set('search', new Set([
+      { value: 'kitten', comment: 'Find kittens' }
+    ]))
+  const result = fixed(params)
+  t.deepEqual(note.firstCall.args[0], params)
+  t.is(comment.firstCall.args[0], '-search- Find kittens')
+  t.true(result.startsWith(`// -search- Find kittens`))
 })
