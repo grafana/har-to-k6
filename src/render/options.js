fe4bbbc94bf3a67028fa5c8e27c2c2a20eef851d
Simon Legander <simon.legander@gmail.com>
2019-05-17 17:36:17 +0200

    Add options renderer.

--- src/render/options.js
+++ src/render/options.js
@@ -1,5 +1,12 @@
 function options (result) {
-  return null
+  const obj = {};
+
+  for (const [key, value] of result.options) {
+    obj[key] = value
+  }
+
+  return `export const options = ${JSON.stringify(obj)}`;
 }
+
 
 module.exports = options
