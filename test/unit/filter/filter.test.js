6d3b722bccfc4407b9fab28b7b97dd16caf08106
Bryan Larsen <blarsen@threekit.com>
2020-09-29 15:40:52 -0400

    [test] add test for --only

---
+++ test/unit/filter/filter.test.js
@@ -0,0 +1,36 @@
+import test from 'ava'
+import filter from 'filter'
+
+function makeFixture(urls) {
+  return {
+    log: {
+      entries: urls.map((url) => {
+        return {
+          request: { url },
+        }
+      }),
+    },
+  }
+}
+
+test('nil', async (t) => {
+  const fixture = makeFixture(['https://example.com/'])
+  const compare = makeFixture(['https://example.com/'])
+  t.deepEqual(compare, await filter(compare, { only: null }))
+})
+
+test('filter', async (t) => {
+  const fixture = makeFixture([
+    'https://example.com/foo',
+    'https://bar.com/bat',
+    'https://bat.com/boo',
+  ])
+  const compare = makeFixture([
+    'https://example.com/foo',
+    'https://bat.com/boo',
+  ])
+  t.deepEqual(
+    compare,
+    await filter(compare, { only: ['example.com', 'bat.com'] })
+  )
+})
