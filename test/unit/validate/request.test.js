c2813589a00b5c624de1485f3d01f8166c108396
Johan Suleiko Allansson <johan@loadimpact.com>
2020-12-11 10:33:06 +0100

    [refactor] don't use isolate in validate/request tests

--- test/unit/validate/request.test.js
+++ test/unit/validate/request.test.js
@@ -1,61 +1,50 @@
 import test from 'ava'
-import isolate from 'helper/isolate'
 import { assay as makeAssay } from 'make'
-const [request, { cookies, headers, postData, queryString }] = isolate(
-  test,
-  'validate/request',
-  {
-    cookies: 'validate/cookies',
-    headers: 'validate/headers',
-    postData: 'validate/postData',
-    queryString: 'validate/queryString',
-  }
-)
+import request from 'validate/request'
 
-test.serial('missing method', (t) => {
-  t.throws(
-    () => {
-      request({}, 0, makeAssay())
-    },
-    { name: 'MissingRequestMethod' }
-  )
-})
-
-test.serial('invalid method', (t) => {
-  t.throws(
-    () => {
-      request({ method: 5 }, 0, makeAssay())
-    },
-    { name: 'InvalidRequestMethod' }
-  )
-})
-
-test.serial('missing url', (t) => {
-  t.throws(
-    () => {
-      request({ method: 'GET' }, 0, makeAssay())
-    },
-    { name: 'MissingRequestUrl' }
-  )
-})
-
-test.serial('invalid url type', (t) => {
-  t.throws(
-    () => {
-      request({ method: 'GET', url: 5 }, 0, makeAssay())
-    },
+const init = (requestBody) =>
+  Object.assign(
+    {},
     {
-      name: 'InvalidRequestUrl',
-      message: 'Invalid request url (0): must be string',
-    }
+      method: 'GET',
+      url: 'http://example.com',
+      queryString: [],
+      headers: [],
+      cookies: [],
+      postData: {},
+      comment: 'Load front page',
+    },
+    requestBody
   )
+
+test('missing method', (t) => {
+  t.throws(() => request(init({ method: null }), 0, makeAssay()), {
+    name: 'MissingRequestMethod',
+  })
 })
 
-test.serial('invalid url format', (t) => {
+test('invalid method', (t) => {
+  t.throws(() => request(init({ method: 5 }), 0, makeAssay()), {
+    name: 'InvalidRequestMethod',
+  })
+})
+
+test('missing url', (t) => {
+  t.throws(() => request(init({ method: 'GET', url: null }), 0, makeAssay()), {
+    name: 'MissingRequestUrl',
+  })
+})
+
+test('invalid url type', (t) => {
+  t.throws(() => request(init({ method: 'GET', url: 5 }), 0, makeAssay()), {
+    name: 'InvalidRequestUrl',
+    message: 'Invalid request url (0): must be string',
+  })
+})
+
+test('invalid url format', (t) => {
   t.throws(
-    () => {
-      request({ method: 'GET', url: 'example.com' }, 0, makeAssay())
-    },
+    () => request(init({ method: 'GET', url: 'example.com' }), 0, makeAssay()),
     {
       name: 'InvalidRequestUrl',
       message:
@@ -64,9 +53,9 @@ test.serial('invalid url format', (t) => {
   )
 })
 
-test.serial('invalid queryString', (t) => {
+test('invalid queryString', (t) => {
   t.throws(
-    () => {
+    () =>
       request(
         {
           method: 'GET',
@@ -75,15 +64,14 @@ test.serial('invalid queryString', (t) => {
         },
         0,
         makeAssay()
-      )
-    },
+      ),
     { name: 'InvalidRequestQuery' }
   )
 })
 
-test.serial('invalid headers', (t) => {
+test('invalid headers', (t) => {
   t.throws(
-    () => {
+    () =>
       request(
         {
           method: 'GET',
@@ -92,15 +80,14 @@ test.serial('invalid headers', (t) => {
         },
         0,
         makeAssay()
-      )
-    },
+      ),
     { name: 'InvalidRequestHeaders' }
   )
 })
 
-test.serial('invalid cookies', (t) => {
+test('invalid cookies', (t) => {
   t.throws(
-    () => {
+    () =>
       request(
         {
           method: 'GET',
@@ -109,15 +96,14 @@ test.serial('invalid cookies', (t) => {
         },
         0,
         makeAssay()
-      )
-    },
+      ),
     { name: 'InvalidRequestCookies' }
   )
 })
 
-test.serial('invalid postData', (t) => {
+test('invalid postData', (t) => {
   t.throws(
-    () => {
+    () =>
       request(
         {
           method: 'GET',
@@ -126,15 +112,14 @@ test.serial('invalid postData', (t) => {
         },
         0,
         makeAssay()
-      )
-    },
+      ),
     { name: 'InvalidRequestData' }
   )
 })
 
-test.serial('invalid comment', (t) => {
+test('invalid comment', (t) => {
   t.throws(
-    () => {
+    () =>
       request(
         {
           method: 'GET',
@@ -143,13 +128,12 @@ test.serial('invalid comment', (t) => {
         },
         0,
         makeAssay()
-      )
-    },
+      ),
     { name: 'InvalidComment' }
   )
 })
 
-test.serial('GET with body', (t) => {
+test('GET with body', (t) => {
   t.throws(
     () => {
       request(
@@ -166,7 +150,7 @@ test.serial('GET with body', (t) => {
   )
 })
 
-test.serial('inconsistent Content-Type', (t) => {
+test('inconsistent Content-Type', (t) => {
   t.throws(
     () => {
       request(
@@ -184,8 +168,8 @@ test.serial('inconsistent Content-Type', (t) => {
   )
 })
 
-test.serial('consistent Content-Type', (t) => {
-  t.notThrows(() => {
+test('consistent Content-Type', (t) => {
+  t.notThrows(() =>
     request(
       {
         method: 'POST',
@@ -196,58 +180,48 @@ test.serial('consistent Content-Type', (t) => {
       0,
       makeAssay()
     )
+  )
+})
+
+test('valid http url', (t) => {
+  t.notThrows(() => {
+    request(init({ method: 'GET', url: 'http://example.com' }), 0, makeAssay())
   })
 })
 
-test.serial('valid http url', (t) => {
-  request({ method: 'GET', url: 'http://example.com' }, 0, makeAssay())
-  t.true(queryString.notCalled)
-  t.true(headers.notCalled)
-  t.true(cookies.notCalled)
-  t.true(postData.notCalled)
+test('valid https url', (t) => {
+  t.notThrows(() => {
+    request(init({ method: 'GET', url: 'https://example.com' }), 0, makeAssay())
+  })
 })
 
-test.serial('valid https url', (t) => {
-  request({ method: 'GET', url: 'https://example.com' }, 0, makeAssay())
-  t.true(queryString.notCalled)
-  t.true(headers.notCalled)
-  t.true(cookies.notCalled)
-  t.true(postData.notCalled)
+test('valid ftp url', (t) => {
+  t.notThrows(() => {
+    request(init({ method: 'GET', url: 'ftp://example.com' }), 0, makeAssay())
+  })
 })
 
-test.serial('valid ftp url', (t) => {
-  request({ method: 'GET', url: 'ftp://example.com' }, 0, makeAssay())
-  t.true(queryString.notCalled)
-  t.true(headers.notCalled)
-  t.true(cookies.notCalled)
-  t.true(postData.notCalled)
-})
-
-test.serial('valid variable url', (t) => {
+test('valid variable url', (t) => {
   /* eslint-disable-next-line no-template-curly-in-string */
-  request({ method: 'GET', url: '${base}/index.html' }, 0, makeAssay())
-  t.true(queryString.notCalled)
-  t.true(headers.notCalled)
-  t.true(cookies.notCalled)
-  t.true(postData.notCalled)
+  t.notThrows(() => {
+    request(init({ method: 'GET', url: '${base}/index.html' }), 0, makeAssay())
+  })
 })
 
-test.serial('valid full', (t) => {
-  request(
-    {
-      method: 'GET',
-      url: 'http://example.com',
-      queryString: [],
-      headers: [],
-      cookies: [],
-      postData: {},
-      comment: 'Load front page',
-    },
-    0,
-    makeAssay()
-  )
-  t.true(queryString.calledOnce)
-  t.true(headers.calledOnce)
-  t.true(cookies.calledOnce)
-  t.true(postData.calledOnce)
+test('valid full', (t) => {
+  t.notThrows(() => {
+    request(
+      init({
+        method: 'GET',
+        url: 'http://example.com',
+        queryString: [],
+        headers: [],
+        cookies: [],
+        postData: {},
+        comment: 'Load front page',
+      }),
+      0,
+      makeAssay()
+    )
+  })
 })
