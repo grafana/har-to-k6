78efb7b87bd0969833000444049c4ac08eabed3d
Edvinas Daugirdas <edvinas.daugirdas@gmail.com>
2021-02-18 14:23:46 +0100

    [refactor] Use map rather than forEach

--- src/render/post/multipart/fixed/pre.js
+++ src/render/post/multipart/fixed/pre.js
@@ -11,28 +11,30 @@ function generateMultipartFixedPre(params, boundary) {
 }
 
 function generateAppendData(params) {
-  return Array.from(params).reduce((acc, [name, valueSet]) => {
-    valueSet.forEach((value) => {
-      const data = `data: ${text(value.value)}`
+  return Array.from(params).reduce(
+    (acc, [name, valueSet]) => [
+      ...acc,
+      ...Array.from(valueSet).map((value) => {
+        const data = `data: ${text(value.value)}`
 
-      let fileName = ''
-      let contentType = `, content_type: ${text('text/plain')}`
+        let fileName = ''
+        let contentType = `, content_type: ${text('text/plain')}`
 
-      if (value.fileName) {
-        fileName = `, filename: ${text(value.fileName)}`
-      }
+        if (value.fileName) {
+          fileName = `, filename: ${text(value.fileName)}`
+        }
 
-      if (value.type) {
-        contentType = `, content_type: ${text(value.type)}`
-      }
+        if (value.type) {
+          contentType = `, content_type: ${text(value.type)}`
+        }
 
-      acc.push(
-        `formData.append(${text(name)}, {${data}${fileName}${contentType}});`
-      )
-    })
-
-    return acc
-  }, [])
+        return `formData.append(${text(
+          name
+        )}, {${data}${fileName}${contentType}});`
+      }),
+    ],
+    []
+  )
 }
 
 module.exports = generateMultipartFixedPre
