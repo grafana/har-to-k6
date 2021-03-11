fd16fea5567aa5bf49e727139d394c9792b463c7
Thomas Wikman <thomas@k6.io>
2021-03-11 21:41:12 +0100

    [fix] Care a little more about archive structure

--- src/normalize/index.js
+++ src/normalize/index.js
@@ -2,7 +2,7 @@ const Normalizer = require('./Normalizer')
 const { DEFAULT_OPTIONS } = require('../constants')
 
 function normalize(archive, options = DEFAULT_OPTIONS) {
-  if (archive) {
+  if (archive && archive.log) {
     const normalizer = new Normalizer(archive, options)
     return normalizer.getArchive()
   }
